ccb46d36d21dbeb263dd4215d615ad664c3801b7
Maddy Randle <maddyrandle318@gmail.com>
2022-08-11 17:51:25 -0500

    Complete Objects warmup exercise

--- src/warm-up.js
+++ src/warm-up.js
@@ -81,7 +81,12 @@ console.log(fruits[4]);
 // keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
 // the value for isOpen should be true. Which other key value pairs would make sense to add to this object?
 
-
+let groceryStore = {
+  name: "King Soopers",
+  isOpen: true
+}
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+groceryStore.location = 'Denver, Colorado';
