6b56f6ee6aae4d49752007d81694dc17ac44f7b8
Maddy Randle <maddyrandle318@gmail.com>
2022-08-15 18:10:27 -0500

    Pass restaurant unit tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,33 @@
+function createRestaurant(name) {
+  return pizzaRestaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    },
+  }
+}
 
+function addMenuItem(pizzaRestaurant, menuItem) {
+  let itemType = menuItem.type
+  !pizzaRestaurant.menus[itemType].includes(menuItem) &&
+    pizzaRestaurant.menus[itemType].push(menuItem);
+}
+
+function removeMenuItem(pizzaRestaurant, menuItemName, itemType) {
+ if(pizzaRestaurant.menus[itemType].length === 0) {
+   return `Sorry, we don't sell ${menuItemName}, try adding a new recipe!`;
+ } else {
+    for (var i = 0; i <= pizzaRestaurant.menus[itemType].length; i++) {
+      pizzaRestaurant.menus[itemType].splice(pizzaRestaurant.menus[itemType][i], 1);
+      return `No one is eating our ${menuItemName} - it has been removed from the ${itemType} menu!`;
+     }
+ }
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
