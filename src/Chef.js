78a5f1c0ee5c2d42d62e32e7f868622b8ccaa7dc
Maddy Randle <maddyrandle318@gmail.com>
2022-08-15 21:12:51 -0500

    Pass chef.js tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,30 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+
+  }
+
+  greetCustomer(customerName, morningTime) {
+    if(morningTime === true) {
+      return "Good morning, " + customerName + "!";
+    } else {
+      return "Hello, " + customerName + "!";
+    }
+  }
+
+  checkForFood(foodItem) {
+    let menu = this.restaurant.menus;
+    let menuItemType = foodItem.type;
+    let menuItemTypeArr = menu[menuItemType];
+
+    if(menuItemTypeArr.length === 0) {
+      return `Sorry, we aren't serving ${foodItem.name} today.`;
+    } else {
+       for (var i = 0; i <= menuItemTypeArr.length; i++) {
+         return `Yes, we're serving ${foodItem.name} today!`;
+       };
+    };
   }
 }
 
